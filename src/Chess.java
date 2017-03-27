0ef014fc922c7609131da65adeaad6de8d66fdf4
Brendon Hollingsworth <brendon.r.hollingsworth@outlook.com>
2017-03-27 11:14:10 -0400

    Initial commit

---
+++ src/Chess.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Hollingsworth on 3/27/2017.
+ */
+public class Chess {
+    public static void main(String[] args){
+        System.out.println("test output");
+        test2.callme();
+    }
+}
---
+++ src/test2.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Hollingsworth on 3/27/2017.
+ */
+public class test2 {
+    public static void callme(){
+        System.out.println("Test2");
+    }
+}
