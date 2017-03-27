074c5ba01a0f32d1a3ce8aa6077483571da3f469
Brendon Hollingsworth <Zeheva@users.noreply.github.com>
2017-03-27 11:19:08 -0400

    Update Chess.java

--- src/Chess.java
+++ src/Chess.java
@@ -3,7 +3,7 @@
  */
 public class Chess {
     public static void main(String[] args){
-        System.out.println("test output");
+        System.out.println("test outp3434344ut");
         test2.callme();
     }
 }
